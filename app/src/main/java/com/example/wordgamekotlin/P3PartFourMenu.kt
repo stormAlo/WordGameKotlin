4f8b128e4b858994cbaf5e42614bb023d6df2ac4
mahdieh <mahdiehhabibi90@gmail.com>
2025-01-24 14:27:10 +0330

    دیزاین مرحله چهارم (کدها وارد نشده)

---
+++ app/src/main/java/com/example/wordgamekotlin/P3PartFourMenu.kt
@@ -0,0 +1,20 @@
+package com.example.wordgamekotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class P3PartFourMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_p3_part_four_menu)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
