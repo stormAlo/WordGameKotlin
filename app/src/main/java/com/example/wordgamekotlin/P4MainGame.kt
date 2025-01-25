594c23728acc95c6ad249e9a3a21452303ec1c3e
Qazal <aa2003131aa@gmail.com>
2025-01-25 15:24:10 +0330

    ساخت صفحه اصلی بازی و شرط های مربوط به آن

--- app/src/main/java/com/example/wordgamekotlin/P4MainGame.kt
+++ app/src/main/java/com/example/wordgamekotlin/P4MainGame.kt
@@ -1,20 +1,180 @@
 package com.example.wordgamekotlin
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import java.util.*
 
 class P4MainGame : AppCompatActivity() {
+
+    private lateinit var dbHelper: WordDatabaseHelper
+    private lateinit var wordContainer: GridLayout
+    private lateinit var spaceContainer: GridLayout
+    private val wordButtons = mutableListOf<Button>()
+    private val spaceTextViews = mutableListOf<TextView>()
+    private lateinit var thisStageWord: String
+    private lateinit var resetButton: Button
+    private val characters = mutableListOf<String>()
+    private lateinit var levelTextView: TextView
+    private var completeWord = StringBuilder()
+    private var lastTextView: TextView? = null
+    private var levelNumber: Int = 0
+    private var stageNumber: Int = 0
+    private var liveStage: Int = 5
+    private lateinit var heart: Array<ImageView>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_p4_main_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        dbHelper = WordDatabaseHelper(this)
+        wordContainer = findViewById(R.id.wordContainer)
+        spaceContainer = findViewById(R.id.spaceContainer)
+        resetButton = findViewById(R.id.resetButton)
+        levelTextView = findViewById(R.id.levelTextView)
+
+        heart = arrayOf(
+            findViewById(R.id.heart1img),
+            findViewById(R.id.heart2img),
+            findViewById(R.id.heart3img),
+            findViewById(R.id.heart4img),
+            findViewById(R.id.heart5img)
+        )
+
+        levelNumber = intent.getIntExtra("LEVEL_NUMBER", 0)
+        stageNumber = intent.getIntExtra("STAGE_NUMBER", 0)
+
+        levelTextView.text = "مرحله: ${levelNumber + 1}"
+
+        thisStageWord = when (stageNumber) {
+            1 -> dbHelper.makingLists(levelNumber, 1)
+            2 -> dbHelper.makingLists(levelNumber, 2)
+            3 -> dbHelper.makingLists(levelNumber, 3)
+            else -> ""
         }
+
+        loadWords()
+
+        resetButton.setOnClickListener { resetWords() }
+    }
+
+    private fun loadWords() {
+        for (i in thisStageWord.indices) {
+            val currentChar = thisStageWord[i]
+            characters.add(currentChar.toString())
+        }
+        characters.shuffle()
+        spaceTextViews.clear()
+
+        wordContainer.removeAllViews()
+        spaceContainer.removeAllViews()
+
+        for (character in characters) {
+            createWordButton(character)
+            createSpaceTextView()
+        }
+    }
+
+    private fun createSpaceTextView() {
+        val wordText = TextView(this).apply {
+            text = "___ "
+            textSize = 20f
+            setTextColor(Color.WHITE)
+        }
+
+        val params = GridLayout.LayoutParams().apply {
+            width = GridLayout.LayoutParams.WRAP_CONTENT
+            height = GridLayout.LayoutParams.WRAP_CONTENT
+        }
+
+        wordText.layoutParams = params
+        spaceContainer.addView(wordText)
+        spaceTextViews.add(wordText)
+    }
+
+    private fun createWordButton(word: String) {
+        val wordButton = Button(this).apply {
+            text = word
+            textSize = 16f
+            layoutParams = GridLayout.LayoutParams().apply {
+                width = (40 * resources.displayMetrics.density).toInt()
+                height = GridLayout.LayoutParams.WRAP_CONTENT
+            }
+            setOnClickListener { replaceWordWithLetter(this) }
+        }
+
+        wordContainer.addView(wordButton)
+        wordButtons.add(wordButton)
+    }
+
+    private fun replaceWordWithLetter(wordButton: Button) {
+        val letter = wordButton.text.toString()
+        completeWord.append(letter)
+
+        for (spaceTextView in spaceTextViews) {
+            if (spaceTextView.text.toString() == "___ ") {
+                lastTextView?.let { spaceContainer.removeView(it) }
+                spaceTextView.text = completeWord.toString()
+                lastTextView = spaceTextView
+                break
+            }
+        }
+        wordButton.isClickable = false
+        wordButton.alpha = 0.5f
+
+        if (completeWord.length == thisStageWord.length) {
+            if (thisStageWord == completeWord.toString()) {
+                winningEvents()
+            }
+//            else {
+//                losingHeart()
+//            }
+        }
+    }
+
+    private fun resetWords() {
+        wordContainer.removeAllViews()
+        spaceContainer.removeAllViews()
+
+        characters.clear()
+        completeWord = StringBuilder()
+        lastTextView = null
+
+        loadWords()
+    }
+
+
+
+    private fun winningEvents() {
+        Toast.makeText(this, "آفرین!", Toast.LENGTH_SHORT).show()
+        resetButton.isEnabled = false
+        resetButton.alpha = 0.5f
+        val newScore = ScoreManager.getInstance().score + 1
+        ScoreManager.getInstance().setScore(newScore)
+
+        Handler().postDelayed({
+            when (stageNumber) {
+                1 -> startActivity(Intent(this, P3PartOneMenu::class.java))
+                2 -> startActivity(Intent(this, P3PartTwoMenu::class.java))
+                3 -> startActivity(Intent(this, P3PartThreeMenu::class.java))
+            }
+            finish()
+        }, 1000)
+
     }
 }
